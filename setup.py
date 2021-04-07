1e1741b1639603f80d5a92399d98ca26a3195bf4
P-Contorta <nkarmstrong@ucdavis.edu>
2021-04-07 14:42:08 -0600

    bump setup.py to version 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description()->str:
 
 setuptools.setup(
     name="UnitCalc",
-    version="1.0.0",
+    version="1.0.1",
     author="Nicholas Armstrong",
     author_email="nkarmstrong@ucdavis.edu",
     description="A Python Library for doing Scientific/Engineering mathematics!",
