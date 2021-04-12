ee2d12fbe4cb15131bdc2aaab458c1fa9242d83c
P-Contorta <nkarmstrong@ucdavis.edu>
2021-04-12 17:35:41 -0600

    Fixed coulomb _re_string

--- value_types/complex_values/charge.py
+++ value_types/complex_values/charge.py
@@ -1,7 +1,7 @@
 from value_types.value import SIValue
 
 class Coulomb(SIValue):
-    _re_pattern = "[Cc]oul(omb)?s?$"
+    _re_pattern = "^[Cc]oul(omb)?s?$"
     
     def __init__(self,value,prefix:str=""):
         super().__init__(value,prefix=prefix,A=1,sec=1)
