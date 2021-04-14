51b90e47ac7ef0c72c872b1d66d96b2ef6af3943
P-Contorta <nkarmstrong@ucdavis.edu>
2021-04-13 23:15:45 -0600

    Added PhysicsParser to __init__.py in unitcalc/

--- src/unitcalc/__init__.py
+++ src/unitcalc/__init__.py
@@ -2,3 +2,4 @@ from .units import Units
 from .constants import Constants
 from .physics import Physics
 from .unit_parser.electrictian_parser import ElectricianParser
+from .unit_parser.physics_parser import PhysicsParser
