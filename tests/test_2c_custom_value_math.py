724fff68c6f96166dbf6bfe1582ffde172403ac3
P-Contorta <nkarmstrong@ucdavis.edu>
2021-04-15 16:47:58 -0600

    tests in math are dependent on tests passing in bool. List bool tests to be run first

--- tests/test_2c_custom_value_math.py
+++ tests/test_2c_custom_value_math.py
@@ -68,3 +68,66 @@ class CustomValueAdd(unittest.TestCase):
 
 
 class CustomValueSub(unittest.TestCase):
+    cv_1 = CustomValue(1,kg=1,m=2)
+    cv_2 = CustomValue(2,kg=1,m=2)
+    cv_dimless = CustomValue(3.14)
+    const_int = 5
+    const_float = 5.
+
+    def test_sub_same_units(self):
+        ans = CustomValue(-1,kg=1,m=2)
+        test_sum = CustomValueAdd.cv_1 - CustomValueAdd.cv_2
+        self.assertEqual(ans,test_sum)
+
+
+    def test_sub_dimensionless_and_const_int(self):
+        ans = CustomValue(-1.86)
+        test_sum = CustomValueAdd.cv_dimless - CustomValueAdd.const_int
+        self.assertAlmostEqual(ans,test_sum)
+
+    def test_sub_dimensionless_and_const_float(self):
+        ans = CustomValue(-1.86)
+        test_sum = CustomValueAdd.cv_dimless - CustomValueAdd.const_float
+        self.assertAlmostEqual(ans,test_sum)
+
+
+    def test_sub_diff_units_dimless(self):
+        with self.assertRaises(ValueError):
+            CustomValueAdd.cv_1 - CustomValueAdd.cv_dimless
+
+    def test_sub_diff_units_int(self):
+        with self.assertRaises(ValueError):
+            CustomValueAdd.cv_1 - CustomValueAdd.const_int
+
+    def test_sub_diff_units_float(self):
+        with self.assertRaises(ValueError):
+            CustomValueAdd.cv_1 - CustomValueAdd.const_float
+
+
+    def test_sub_invalid(self):
+        with self.assertRaises(AttributeError):
+            CustomValueAdd.cv_1 - "invalid"
+
+
+    def test_rsub_dimensionless_and_const_int(self):
+        ans = CustomValue(1.86)
+        test_sum = CustomValueAdd.const_int - CustomValueAdd.cv_dimless
+        self.assertAlmostEqual(ans,test_sum)
+
+    def test_rsub_dimensionless_and_const_float(self):
+        ans = CustomValue(1.86)
+        test_sum = CustomValueAdd.const_float - CustomValueAdd.cv_dimless
+        self.assertAlmostEqual(ans,test_sum)
+
+
+    def test_rsub_diff_units_int(self):
+        with self.assertRaises(ValueError):
+            CustomValueAdd.const_int - CustomValueAdd.cv_1
+
+    def test_sub_diff_units_float(self):
+        with self.assertRaises(ValueError):
+            CustomValueAdd.const_float - CustomValueAdd.cv_1
+
+    def test_rsub_invalid(self):
+        with self.assertRaises(AttributeError):
+            "invalid" - CustomValueAdd.cv_1
