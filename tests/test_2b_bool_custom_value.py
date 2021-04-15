9dc290c343448d2d7cd8f2b3c9efb63b47ef67e5
P-Contorta <nkarmstrong@ucdavis.edu>
2021-04-15 17:49:48 -0600

    CustomValue ge tests finalized.

--- tests/test_2b_bool_custom_value.py
+++ tests/test_2b_bool_custom_value.py
@@ -63,48 +63,85 @@ class CustomValueLessThan(unittest.TestCase):
     def test_lt_same_units_false(self):
         self.assertFalse(CustomValue(4.56,kg=1,A=-1) < CustomValue(1.23,kg=1,A=-1))
 
+
     def test_lt_dimless_and_int_true(self):
         self.assertTrue(CustomValue(1) < 2)
-
 
     def test_lt_dimless_and_int_false(self):
         self.assertFalse(CustomValue(2) < 1)
 
+
+
     def test_lt_dimless_and_float_true(self):
         self.assertTrue(CustomValue(1.234) < 4.321)
 
-
     def test_lt_dimless_and_float_false(self):
         self.assertFalse(CustomValue(4.321) < 1.234)
+
 
     def test_lt_invalid(self):
         with self.assertRaises(TypeError):
             CustomValue(4.321) < "invalid"
 
 
-
-
 class CustomValueGreaterThanEqual(unittest.TestCase):
-    def test_lt_same_units_true(self):
+    def test_ge_same_units_false(self):
         self.assertFalse(CustomValue(1.23,kg=1,A=-1) >= CustomValue(4.56,kg=1,A=-1))
 
-    def test_lt_same_units_false(self):
+    def test_ge_same_units_true(self):
         self.assertTrue(CustomValue(4.56,kg=1,A=-1) >= CustomValue(1.23,kg=1,A=-1))
 
-    def test_lt_dimless_and_int_true(self):
+
+    def test_ge_dimless_and_int_false(self):
         self.assertFalse(CustomValue(1) >= 2)
 
-
-    def test_lt_dimless_and_int_false(self):
+    def test_ge_dimless_and_int_true(self):
         self.assertTrue(CustomValue(2) >= 1)
 
-    def test_lt_dimless_and_float_true(self):
+
+
+    def test_ge_dimless_and_float_false(self):
         self.assertFalse(CustomValue(1.234) >= 4.321)
 
-
-    def test_lt_dimless_and_float_false(self):
+    def test_ge_dimless_and_float_true(self):
         self.assertTrue(CustomValue(4.321) >= 1.234)
 
-    def test_lt_invalid(self):
+
+    def test_ge_invalid(self):
         with self.assertRaises(TypeError):
             CustomValue(4.321) >= "invalid"
+
+
+class CustomValueGreaterThan(unittest.TestCase):
+    def test_gt_same_units_false(self):
+        self.assertFalse(CustomValue(1.23,kg=1,A=-1) > CustomValue(4.56,kg=1,A=-1))
+
+    def test_gt_same_units_true(self):
+        self.assertTrue(CustomValue(4.56,kg=1,A=-1) > CustomValue(1.23,kg=1,A=-1))
+
+    def test_gt_same_units_eq_false(self):
+        self.assertFalse(CustomValue(1.23,kg=1,A=-1) > CustomValue(1.23,kg=1,A=-1))
+
+    def test_gt_dimless_and_int_false(self):
+        self.assertFalse(CustomValue(1) > 2)
+
+    def test_gt_dimless_and_int_true(self):
+        self.assertTrue(CustomValue(2) >= 1)
+
+    def test_gt_dimless_and_int_eq_false(self):
+        self.assertFalse(CustomValue(2) > 2)
+
+    def test_gt_dimless_and_float_false(self):
+        self.assertFalse(CustomValue(1.234) > 4.321)
+
+    def test_gt_dimless_and_float_true(self):
+        self.assertTrue(CustomValue(4.321) > 1.234)
+
+    def test_gt_dimless_and_float_eq_false(self):
+        self.assertFalse(CustomValue(1.234) > 1.234)
+
+    def test_gt_invalid(self):
+        with self.assertRaises(TypeError):
+            CustomValue(4.321) > "invalid"
+
+
