2428bf417a75f91e60d2371c514e584a6dc51dce
P-Contorta <nkarmstrong@ucdavis.edu>
2021-04-24 12:39:49 -0600

    __gt__ and __lt__ methods in CustomValue were modified so that they return errors if wrong type is passed or units do not match.

--- src/unitcalc/value_types/value.py
+++ src/unitcalc/value_types/value.py
@@ -127,12 +127,18 @@ class CustomValue(object):
             if self.si_units() == other.si_units():
                 if self.central_value() < other.central_value():
                     return True
+                return False
+            else:
+                raise ValueError("Cannot do a less than compare against objects with different units.")
+        elif self.si_units() == Units():
+            if self.value() < other:
+                return True
             return False
         else:
             if self.si_units() == Units():
-                if self.value() < other:
-                    return True
-            return False
+                raise TypeError("Can only do a less than compare against objects with type CustomValue, SIValue, NonSIValue, int, or float.")
+            else:
+                raise TypeError("Can only do a less than compare against objects with type CustomValue, SIValue, or NonSIValue.")
 
     def __ge__(self,other):
         return not self < other
@@ -142,12 +148,18 @@ class CustomValue(object):
             if self.si_units() == other.si_units():
                 if self.central_value() > other.central_value():
                     return True
+                return False
+            else:
+                raise ValueError("Cannot do a greater than compare against objects with different units.")
+        elif self.si_units() == Units():
+            if self.value() > other:
+                return True
             return False
         else:
             if self.si_units() == Units():
-                if self.value() > other:
-                    return True
-            return False
+                raise TypeError("Can only do a greater than compare against objects with type CustomValue, SIValue, NonSIValue, int, or float.")
+            else:
+                raise TypeError("Can only do a greater than compare against objects with type CustomValue, SIValue, or NonSIValue.")
 
     def __le__(self,other):
         return not self > other
